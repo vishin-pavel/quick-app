9f677b981dfda76aa43b928213df7da6fdab8360
vishin-pavel <pavel@vishin.ru>
2013-12-06 12:44:25 +0400

    Немного эстетики

--- www/protected/views/site/index.php
+++ www/protected/views/site/index.php
@@ -4,19 +4,22 @@
         'columns' => array(
             array(
                 'name' => 'date',
-                'header' => 'Дата заполнения'
-            ),
-            array(
-                'name' => 'data.Data.FormData.registrationULData.mails',
-                'header' => 'E-mail адрес',
-            ),
-            array(
-                'name' => 'data.Data.FormData.registrationULData.organization.name',
-                'header' => 'Наименование организации',
+                'header' => 'Дата заполнения',
+                'htmlOptions' => array('style' =>'width: 150px'),
             ),
             array(
                 'name' => 'data.Data.FormData.registrationULData.organization.inn',
                 'header' => 'ИНН',
+                'htmlOptions' => array('style' =>'width: 100px'),
+            ),
+            array(
+                'name' => 'data.Data.FormData.registrationULData.mails',
+                'header' => 'E-mail адрес',
+                'htmlOptions' => array('style' =>'min-width: 120px'),
+            ),
+            array(
+                'name' => 'data.Data.FormData.registrationULData.organization.name',
+                'header' => 'Наименование организации',
             ),
             array(
                 'name' => 'data.Data.FormData.registrationULData.dangerObjects.doTypeNameDetail',
